502c3c6bd32fb22e3379b70d0be918439dd1c9d8
Solwet <rmuhtarov54@gmail.com>
2024-10-07 20:07:40 +0500

    Готовый пример функции

---
+++ examples/exmp6.py
@@ -0,0 +1,9 @@
+def cat(name, color, age):
+    return {'name': name, 'color': color, 'age': age}
+my_cat = cat('Grey', 'Alise', 9)
+print(my_cat)
+
+def cat(name, color, age):
+    return {'name': name, 'color': color, 'age': age}
+my_cat = cat(color = 'Grey', age = 9, name = 'Alise')
+print(my_cat)
