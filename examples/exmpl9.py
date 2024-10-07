f9a6902c17ef954f0cfee97a1d1cd2e6b69edc89
Solwet <rmuhtarov54@gmail.com>
2024-10-07 20:23:11 +0500

    last

---
+++ examples/exmpl9.py
@@ -0,0 +1,10 @@
+def book_list(books, func):
+    for book in books:
+        print(func(book))
+
+books = ['System Design','Python и DevOps','Git. Практическое руководство']
+def book_print(book):
+    return book.upper() + ' - прочитано'
+book_list(books, book_print)
+
+
